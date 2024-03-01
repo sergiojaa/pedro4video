dec86193383384a84ee9f108ac32ec920636c885
sergiojaa <119433399+sergiojaa@users.noreply.github.com>
2024-03-02 01:54:09 +0400

    Add files via upload

---
+++ inputisdakonsoleba.js
@@ -0,0 +1,16 @@
+import './App.css';
+import { useState } from 'react'
+
+function App() {
+  const [inputValue, setInputValue] = useState('')
+  const handleInputChange = (event) =>{
+    console.log(event.target.value)
+  }
+
+  return<div className='App'>
+    <input type='text' onChange={handleInputChange} />
+    {inputValue}
+    </div>
+}
+
+export default App;
