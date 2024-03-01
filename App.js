97a3b3234c60dbd402f5cdd982a039c918135fe0
sergiojaa <119433399+sergiojaa@users.noreply.github.com>
2024-03-02 00:41:56 +0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import { useState } from 'react'
+function App() {
+  const [age, setAge] = useState(0)
+  
+  const increaseAge = () =>{
+    setAge(age + 1)
+  };
+  return<div className='App'>
+    {age} 
+    <button onClick={increaseAge} >increase age</button> 
+    </div>
+}
+
+export default App;
